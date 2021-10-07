8d2d96ebde41ee8914e0058c82830207223b3eee
marli <marliferreirapassos@gmail.com>
2021-10-07 14:39:25 -0300

    coloquei no resposivo

--- components/conteudo.tsx
+++ components/conteudo.tsx
@@ -3,7 +3,7 @@ import { IoIosClose } from 'react-icons/io'
 
 const Conteudo = () => {
   return (
-    <div className="w-full flex flex-wrap">
+    <div className="w-full flex flex-wrap md:6/12 lg:w-6/12">
       <div className="w-2/12 bg-gray-200 ">
         <h3 className="text-blue-500 mb-4 mt-4 ml-2 ">Saúde com agente</h3>
 
@@ -18,9 +18,11 @@ const Conteudo = () => {
         </ul>
       </div>
 
-      <div className="w-10/12 flex-wrap bg-white px-24 py-6">
+      <div className="w-10/12 flex-wrap bg-white px-24 py-6 w-full-5/10 md:w-10/12 lg:w-10/12">
         <div className="border-2">
-          <IoIosClose size={24} className="inline" />
+          <div className="flex justify-end">
+            <IoIosClose size={24} className="inline" />
+          </div>
           <p className="text-blue-500 text-2xl pl-4 pt-4">Bem-Vindo</p>
           <p className="text-sm p-4">
             No Cursso Saúde com agente você aprenderá a promover saúde e
@@ -35,25 +37,35 @@ const Conteudo = () => {
             <li className="mb-4 mt-2">Módulo 1 - Epidemiologia</li>
             <li className="text-sm bg-gray-200 p-2 mb-4">
               Aula 1 - Aspectos Epdemiológicos
-              <HiOutlineChevronDown className="inline" />
+              <div className="flex justify-end">
+                <HiOutlineChevronDown className="inline" />
+              </div>
             </li>
 
             <li className="text-sm bg-gray-200 p-2 mb-4">
               Aula 2 - Situação atual ou perspectiva da vacinação da COVID 19
-              <HiOutlineChevronDown className="inline" />
+              <div className="flex justify-end">
+                <HiOutlineChevronDown className="inline" />
+              </div>
             </li>
 
             <li className="text-sm bg-gray-200 p-2 mb-4">
               Aula 3 - Bases imunológicas
-              <HiOutlineChevronDown className="inline" />
+              <div className="flex justify-end">
+                <HiOutlineChevronDown className="inline" />
+              </div>
             </li>
             <li className="text-sm bg-gray-200 p-2 mb-4">
               Aula 4 - Programa Nacional de imunizações
-              <HiOutlineChevronDown className="inline" />
+              <div className="flex justify-end">
+                <HiOutlineChevronDown className="inline" />
+              </div>
             </li>
             <li className="text-sm bg-gray-200 p-2">
               Aula 5 - Cuidade em saúde
-              <HiOutlineChevronDown className="inline" />
+              <div className="flex justify-end">
+                <HiOutlineChevronDown className="inline" />
+              </div>
             </li>
           </ul>
         </div>
@@ -62,12 +74,16 @@ const Conteudo = () => {
             <li className="mb-4 mt-4">Módulo 2 - Imunização</li>
             <li className="text-sm bg-gray-200 p-2 mb-4">
               Aula 1 - Aspectos Epdemiológicos
-              <HiOutlineChevronDown className="inline" />
+              <div className="flex justify-end">
+                <HiOutlineChevronDown className="inline" />
+              </div>
             </li>
             <li className="text-sm bg-gray-200 p-2 mb-4">
               Aula 2 - Situação atual ou perspectiva da vacinação da COVID 19 no
               Brasil
-              <HiOutlineChevronDown className="inline" />
+              <div className="flex justify-end">
+                <HiOutlineChevronDown className="inline" />
+              </div>
             </li>
           </ul>
         </div>
--- components/menu.tsx
+++ components/menu.tsx
@@ -1,12 +1,18 @@
 import { FaRegBell } from 'react-icons/fa'
+import { HiOutlineChevronDown } from 'react-icons/hi'
 
 const Menu = () => {
   return (
-    <div className="flex flex-wrap items-center">
-      <img src="/log.jpeg" alt="logo" className="w-40" />
-      <span className="test-2xl">Cursos</span>
-      <FaRegBell className="inline" />
-      <span className="pl-2"> (__) Fracisca Moraes </span>
+    <div className="w-full flex flex-wrap items-center w-full: md:1/12 lg:2/12">
+      <div className="w-2/12">
+        <img src="/log.jpeg" alt="logo" className="w-40" />
+      </div>
+      <div className="w-10/12 flex flex-wrap justify-end w-full-hiden md:block lg:block">
+        <span className="mr-4">Cursos</span>
+        <FaRegBell size={24} className="inline pl-2" />
+        <span className="pl-4"> (__) Fracisca Moraes </span>
+        <HiOutlineChevronDown size={24} className="inline pl-2 pr-4" />
+      </div>
     </div>
   )
 }
