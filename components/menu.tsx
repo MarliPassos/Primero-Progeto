64aae48c764ad43feee6eb4c7d735d420fc052a8
marli <marliferreirapassos@gmail.com>
2021-10-15 17:12:20 -0300

    m

--- components/menu.tsx
+++ components/menu.tsx
@@ -3,11 +3,11 @@ import { HiOutlineChevronDown } from 'react-icons/hi'
 
 const Menu = () => {
   return (
-    <div className="w-full flex flex-wrap items-center w-full: md:1/12 lg:2/12">
+    <div className="w-full flex flex-wrap items-center ">
       <div className="w-2/12">
         <img src="/log.jpeg" alt="logo" className="w-40" />
       </div>
-      <div className="w-10/12 flex flex-wrap justify-end w-full-hiden md:block lg:block">
+      <div className="w-10/12 flex flex-wrap justify-end wfu-6/12 full ">
         <span className="mr-4">Cursos</span>
         <FaRegBell size={24} className="inline pl-2" />
         <span className="pl-4"> (__) Fracisca Moraes </span>
